d3a2785485977c795293c1b3d20b013bd2f3c593
chigolite <nelsonlite5065@gmail.com>
2024-07-27 23:41:36 +0100

    switch to nice ui updated some links

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,4 +1,13 @@
-import { Container, Box, Stack, Chip, Paper, Grid, Fab } from "@mui/material";
+import {
+  Container,
+  Box,
+  Stack,
+  Chip,
+  Paper,
+  Grid,
+  Fab,
+  Typography,
+} from "@mui/material";
 import { ProjectsInfo } from "@/Components/home/projectInfo";
 
 const Projects = () => {
